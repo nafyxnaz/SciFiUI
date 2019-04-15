497fb491db7d709b4cc7eab0e2b3e74d08741334
Nafeesa <nafyxnaz@gmail.com>
2019-04-15 17:40:00 +0100

    Description

---
+++ java/src/ie/tudublin/arc.java
@@ -0,0 +1,24 @@
+import processing.core.PApplet;
+
+public class ARC extends PApplet {
+  
+  public void settings(){
+  float cx;
+  float cy;
+  float radius;
+  float start;
+  float stop;
+  float speed;
+  float theta;
+  float frequency;
+  float s,t;
+  float end;
+  float rMax;
+  float rMin;
+  float strokeIntensity;
+  
+  }
+  
+}
+  
+ 
