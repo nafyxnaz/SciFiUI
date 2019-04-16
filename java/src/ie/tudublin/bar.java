f1406d9052e3d6570b5a20e9601c78d24f5efef7
Nafeesa <nafyxnaz@gmail.com>
2019-04-16 12:11:10 +0100

    NewDay

---
+++ java/src/ie/tudublin/bar.java
@@ -0,0 +1,22 @@
+package ie.tudublin;
+
+import processing.core.PApplet;
+
+
+public class bar  {
+  
+  public void settings (){
+    float x;        
+    float y;
+    float barW;
+    float barH;
+    float random;
+    float time;
+    float bMax;
+    float bMin;
+ 
+ 
+ int change = 0;
+  }
+  
+}
