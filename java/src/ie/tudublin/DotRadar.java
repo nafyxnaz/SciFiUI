462278793785873a899a3fc728e612cbec015d49
Nafeesa <nafyxnaz@gmail.com>
2019-04-21 12:26:51 +0100

    NewDay.AnotherDay

---
+++ java/src/ie/tudublin/DotRadar.java
@@ -0,0 +1,34 @@
+public class RadarDot {
+
+  float dx;
+  float dy;
+  float radius;
+  float speed;
+  float ex;
+  float ey;  
+  BeepDot dot;
+ 
+  public void settings() (float dx, float dy, float radius, float speed){   
+   
+    this.dx = dx;
+   this.dy = dy;
+   this.radius = radius;
+   this.speed = speed;
+    
+  };
+  
+  
+  public void moveDot(){
+    if ( dx < (width / 2 - radius) ){
+      ex = random((width /2 + radius) , width);
+      strokeWeight(10);
+      line(dx,dy,ex,dy);
+
+      }
+    else {
+      ex = random(0, width/2);
+      strokeWeight(10);
+      line(dx,dy,ex,dy);
+    }
+  }
+}
