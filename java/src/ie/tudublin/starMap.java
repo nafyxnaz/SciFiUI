8f98547d889fbd241bf88b418486de8cb4ec2680
Nafeesa <nafyxnaz@gmail.com>
2019-04-21 12:40:13 +0100

    NewDay.AnotherDay

---
+++ java/src/ie/tudublin/starMap.java
@@ -0,0 +1,56 @@
+public class starMap{
+ float x;
+ float y;
+ float interval;
+ float amountW;
+ float amountH;
+ int i;
+ int j;
+ 
+ public starMap(float x, float y){
+  this.x = x;
+  this.y = y;
+  this.interval = width / 32;
+  this.amountW = 15;
+  this.amountH =  8;
+  
+ }
+  
+  public void render(){
+    for(i = 0 ; i < amountW; i++){
+       line(x + (interval * i) , y , x + (interval * i), y +(amountH * interval)); 
+       for( j = 0; j <= amountH; j++){
+         line(x , y + (interval * j) , width / 2 - width / 32, y + (interval * j)); 
+       }  
+  }
+    
+    
+    
+  }
+  
+  public void update(){
+    
+    
+    
+  }
+  
+  
+  public void loadTable(){
+    
+   //Table table = loadTable("planetData.csv");
+    
+    
+    
+  }
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+}
