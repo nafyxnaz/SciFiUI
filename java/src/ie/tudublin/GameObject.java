03e0ac5c7ede4581d4df7e3be451bc78b5ba8a03
Nafeesa <nafyxnaz@gmail.com>
2019-04-24 19:26:37 +0100

    AnotherDay....

---
+++ java/src/ie/tudublin/GameObject.java
@@ -0,0 +1,17 @@
+package ie.tudublin;
+
+public class GameObject
+{
+    private PVector pos;
+    private float radius;
+    public GameObject()
+    {
+    }
+    
+    public void update()
+    {
+    }
+    public void render()
+    {
+    }
+}
